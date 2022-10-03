4ab5fec210aee172fb308c2fb8e88ce4c931a356
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-10-03 20:54:37 +0400

    adding functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 let searchForm = document.querySelector(".search-form");
+document.querySelector('#search-btn').onClick=() =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
