2308d459276a738d0a61daebd232ed025f6a63f1
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-10-05 21:52:21 +0400

    adding functionality to e-commerce project

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 let searchForm = document.querySelector(".search-form");
-document.querySelector('#search-btn').onClick=() =>{
+
+    document.querySelector('#search-btn').onClick=() =>{
     searchForm.classList.toggle('active');
     shoppingCart.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
+
+let shoppingCart = document.querySelector(".shopping-cart");
+
